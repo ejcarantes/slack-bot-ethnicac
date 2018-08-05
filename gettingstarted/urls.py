6730f0a0c00ac3a659e64c88415f38e947691ee4
Ethnica Carantes (IS-PH) <ethnica_carantes@trendmicro.com>
2018-08-05 20:55:45 +0800

    updated url patters in url.py and added a new function in views.py

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import include, url
-from django.urls import path
+
 
 from django.contrib import admin
 admin.autodiscover()
@@ -11,7 +11,5 @@ import hello.views
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
-    path('admin/', admin.site.urls),
+    url(r'^slackbot', hello.views.slack, name='slack'),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,9 @@
+
+import json
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 from .models import Greeting
 
@@ -17,4 +21,11 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, 'db.html', {'greetings': greetings})
+
+@csrf_exempt
+def slack(request):
+    request_body = json.loads(request.body)
+    print (request_body)
+    return ""
+
 
