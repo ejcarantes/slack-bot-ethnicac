a18c2e00a0c4658ebe060351788ff024afc8596b
Ethnica Carantes (IS-PH) <ethnica_carantes@trendmicro.com>
2018-08-07 00:59:19 +0800

    added another script for the schedule of posts

---
+++ clock.py
@@ -0,0 +1,27 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from hello import views
+from hello.views import slackBot, twittertrends
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=1)
+def timed_job():
+    slackBot.api_call(
+        "chat.postMessage",
+        channel="CC2DYCHFT",
+        text=twittertrends(),
+        icon_emoji=':robot_face:'
+    )
+    # print('This job is run every 30 minute.')
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=7)
+def scheduled_job():
+    slackBot.api_call(
+        "chat.postMessage",
+        channel = "CC2DYCHFT",
+        text=twittertrends(),
+        icon_emoji=':robot_face:'
+    )
+    # print('This job is run every weekday at 5pm.')
+
+sched.start()
